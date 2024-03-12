8e26ef4ae35e1ac859c55881e79cb92ae6a9fffc
Fiachra Maguire <fiachra97@hotmail.com>
2024-03-12 22:01:33 +0000

    update missing file saves

--- multinational_retail_data_centralisatio/database_utils.py
+++ multinational_retail_data_centralisatio/database_utils.py
@@ -8,29 +8,30 @@ import pandas as pd
 class DatabaseConnector:
     def __init__(self):
         self.engine = self.init_db_engine()
-        self.credentials = self.read_db_creds()
+        self.upload_engine = self.init_upload_engine()
 
-    def read_db_creds(self):
-        file_path = os.path.join(os.path.dirname(__file__), 'db_creds.yaml')
-        with open(file_path, 'r') as file:
-            credentials = yaml.safe_load(file)
-        return credentials
-    
+    def read_db_creds(self, filename='db_creds.yaml'):
+        with open(filename, 'r') as file:
+            db_creds = yaml.safe_load(file)
+        return db_creds
+
     def init_db_engine(self):
-        engine = create_engine(f"{self.credentials['DATABASE_TYPE']}+{DBAPI}://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}")
-        engine.execution_options(isolation_level='AUTOCOMMIT').connect()
+        creds = self.read_db_creds()
+        db_uri = f"postgresql://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
+        engine = create_engine(db_uri)
+        return engine
 
     def list_db_tables(self):
-        # with self.engine.connect() as connection:
-        #     result = connection.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
-        #     tables = [row[0] for row in result]
-        #     return tables
-        
-        with psycopg2.connect(host=self.credentials['RDS_HOST'], user=RDS_USER, password=RDS_PASSWORD, dbname=RDS_DATABASE, port=RDS_PORT) as conn:
-            with conn.cursor() as cur:
-                cur.execute("""SELECT table_name FROM information_schema.tables
-                WHERE table_schema = 'public'""")
-                for table in cur.fetchall():
-                    print(table)
-                return cur.fetchall()
+        with self.engine.connect() as conn:
+            tables = conn.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+            table_names = [table[0] for table in tables]
+        return table_names
+    
+    def init_upload_engine(self):
+        creds = self.read_db_creds()
+        db_uri = f"postgresql://{creds['USER']}:{creds['PASSWORD']}@{creds['HOST']}:{creds['PORT']}/{creds['DATABASE']}"
+        engine = create_engine(db_uri)
+        return engine
 
+    def upload_to_db(self, df, table_name):
+        df.to_sql(table_name, self.upload_engine, if_exists='replace', index=False)
